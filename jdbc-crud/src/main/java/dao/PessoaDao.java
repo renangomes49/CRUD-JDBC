6250a98fd61bc9ab73706a21dbbd21c21da43e3b
piriquito alegre <piriquito alegre@DESKTOP-8NRMK9H>
2021-06-05 17:35:43 -0300

    Criação do Modelo DAO e a criação dos atributos da Classe Pessoa

--- jdbc-crud/src/main/java/dao/PessoaDao.java
+++ jdbc-crud/src/main/java/dao/PessoaDao.java
@@ -1,5 +1,15 @@
 package dao;
 
+import java.sql.Connection;
+
+import conexaojdbc.Conexao;
+
 public class PessoaDao {
 
+	private Connection connection;
+	
+	public PessoaDao() {
+		connection = Conexao.geConnection();
+	}
+	
 }
--- jdbc-crud/src/main/java/model/Pessoa.java
+++ jdbc-crud/src/main/java/model/Pessoa.java
@@ -2,4 +2,34 @@ package model;
 
 public class Pessoa {
 
+	private Long id;
+	private String nome;
+	private String pessoa;
+
+	
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(String pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 }
